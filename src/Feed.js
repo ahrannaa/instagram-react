0ec01ffe78aa1ce1d4b79cfea323a77b7d6cab55
ahrannaa <ahranna.araujo123@gmail.com>
2022-09-19 21:40:14 +0100

    Projeto finalizado com as configurações de curtidas e salvar post feitas.

--- src/Feed.js
+++ src/Feed.js
@@ -1,77 +1,128 @@
-  function Posts(props){
-    return(
-      <div class="pic-box">
-         <div class="pic-header">
-           <div class="pic-header-profile">
-             <div class="pic-header-profile-img">
-               <img src={props.imagemPerfil} alt="aries-feed" />
-             </div>
-             <h1>{props.nome}</h1>
-           </div>
-           <div class="pic-header-menu">
-             <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
-           </div>
-         </div>
-         <div class="pic">
-           <img src={props.imagemFeed} alt={props.nome} />
-         </div>
-         <div class="pic-footer">
-           <div class="pic-icons-box">
-             <div class="pic-icons-right">
-               <ion-icon name="heart-outline"></ion-icon>
-               <ion-icon name="chatbubble-outline"></ion-icon>
-               <ion-icon name="paper-plane-outline"></ion-icon>
-             </div>
-             <div class="pic-icons-left">
-               <ion-icon name="bookmark-outline"></ion-icon>
-             </div>
-           </div>
-           <div class="likes">
-             <img src={props.imagemLike}alt="respondeai" />
-             <p>
-               Curtido por <span class="keyword">{props.likes}</span> e{" "}
-               <span class="keyword">{props.numLikes}</span>
-             </p>
-           </div>
-         </div>
-       </div>
-    )
-  } 
+import React from "react";
 
+function Posts(props) {
+  const [curtidas, setCurtidas] = React.useState(props.numLikes);
+  const [curtiu, setCurtiu] = React.useState(false);
+  const [iconeCoracao, setIconeCoracao] =  React.useState("heart-outline");
 
+  function inserirCurtidas() {
+    let novasCurtidas = curtidas
+    let classeCoracao = iconeCoracao
+    
+    if (curtiu) {
+      setCurtiu(false);
+      novasCurtidas = novasCurtidas - 1;
+      classeCoracao = "heart-outline"
+    } else {
+      setCurtiu(true);
+      novasCurtidas = novasCurtidas + 1;
+      classeCoracao = "heart"
+    }
 
-export default function Feed(){
+    setCurtidas(novasCurtidas);
+    setIconeCoracao(classeCoracao);
+  }
 
-  const Perfil =[
-    {nome:"Áries", 
-    imagemPerfil:"img/aries.jpeg", 
-    imagemFeed:"img/aries feed novo.webp", 
-    imagemLike:"img/respondeai 2.png",
-    likes:"respondeai",
-    numLikes:"101.523 pessoas" },
+  const[salvar, setSalvar] = React.useState("bookmark-outline");
+  const[favoritou, setFavoritou] = React.useState(false)
+  
+  let classBookmark = salvar
+  
+  function salvarPost(){
+     if(favoritou){
+      setFavoritou(false)
+       classBookmark = "bookmark-outline"
+     } else{
+      setFavoritou(true);
+      classBookmark ="bookmark"
+     }
+    
+     setSalvar(classBookmark)
+  }
 
-    {nome:"Peixes", 
-    imagemPerfil:"img/peixes.png", 
-    imagemFeed:"img/peixes feed.jpg", 
-    imagemLike:"img/adorableanimals 2.png",
-    likes:"adorables_animals",
-    numLikes:"99.159 pessoas" },
+  return (
+    <div class="pic-box">
+      <div class="pic-header">
+        <div class="pic-header-profile">
+          <div class="pic-header-profile-img">
+            <img src={props.imagemPerfil} alt="aries-feed" />
+          </div>
+          <h1>{props.nome}</h1>
+        </div>
+        <div class="pic-header-menu">
+          <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+        </div>
+      </div>
+      <div class="pic">
+        <img src={props.imagemFeed} alt={props.nome} />
+      </div>
+      <div class="pic-footer">
+        <div class="pic-icons-box">
+          <div class="pic-icons-right">
+            <button onClick={inserirCurtidas} class={iconeCoracao}>
+              <ion-icon name={iconeCoracao}></ion-icon>
+            </button>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div class="pic-icons-left">
+           <button onClick={salvarPost}><ion-icon name={salvar}></ion-icon></button>
+          </div>
+        </div>
+        <div class="likes">
+          <img src={props.imagemLike} alt="respondeai" />
+          <p>
+            Curtido por <span class="keyword">{props.likes}</span> e{" "}
+            <span class="keyword">{curtidas} pessoas</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-    {nome:"Leão", 
-    imagemPerfil:"img/leao.png", 
-    imagemFeed:"img/feed leao.jpg", 
-    imagemLike:"img/respondeai 2.png",
-    likes:"respondeai",
-    numLikes:"101.523 pessoas" },
- ]
+export default function Feed() {
+  const Perfil = [
+    {
+      nome: "Áries",
+      imagemPerfil: "img/aries.jpeg",
+      imagemFeed: "img/aries feed novo.webp",
+      imagemLike: "img/respondeai 2.png",
+      likes: "respondeai",
+      numLikes: 101523,
+    },
 
+    {
+      nome: "Peixes",
+      imagemPerfil: "img/peixes.png",
+      imagemFeed: "img/peixes feed.jpg",
+      imagemLike: "img/adorableanimals 2.png",
+      likes: "adorables_animals",
+      numLikes: 99159,
+    },
 
+    {
+      nome: "Leão",
+      imagemPerfil: "img/leao.png",
+      imagemFeed: "img/feed leao.jpg",
+      imagemLike: "img/respondeai 2.png",
+      likes: "respondeai",
+      numLikes: 101523,
+    },
+  ];
 
   return (
     <div class="feed">
-      {Perfil.map((p) => <Posts nome={p.nome} imagemPerfil={p.imagemPerfil} 
-      imagemFeed={p.imagemFeed} imagemLike={p.imagemLike} likes={p.likes} numLikes={p.numLikes}/> )}
-          
-      </div>
-  )
+      {Perfil.map((p) => (
+        <Posts
+          nome={p.nome}
+          imagemPerfil={p.imagemPerfil}
+          imagemFeed={p.imagemFeed}
+          imagemLike={p.imagemLike}
+          likes={p.likes}
+          numLikes={p.numLikes}
+        />
+      ))}
+    </div>
+  );
 }
